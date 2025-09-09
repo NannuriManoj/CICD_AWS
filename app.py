55257a838ac5123dcda507656f230f9217aa3bfe
MANOJ NANNURI <106346544+NannuriManoj@users.noreply.github.com>
2025-09-09 12:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Manoj. This is from flask...!'
+    return 'Hello, Manoj. This is from AWS...!'
 
 if __name__ == '__main__':
     app.run()
